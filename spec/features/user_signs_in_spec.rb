76850a487ce37bad8221d6c65685d7f97cb2c9fc
Brian Akers <brianakers@Brians-MacBook-Pro.local>
2017-09-06 10:17:17 -0400

    Add Devise Sign-Up

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,20 @@
+# require 'spec_helper'
+#
+# feature 'user signs in;
+#   As a user
+#   I want to sign in
+#   So that I can leave reviews' do
+#
+#   scenario 'an existing user specifies a valid username and password' do
+#   end
+#
+#   scenario 'a nonexistant username and password is supplied' do
+#   end
+#
+#   scenario 'an existing username with the wrong password is supplied and denied' do
+#   end
+#
+#   scenario 'an already signed in user cannot re-sign in' do
+#   end
+#
+# end
