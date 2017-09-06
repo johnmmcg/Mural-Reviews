90d1e55b83a51220bd1d96ed68007ac669188a53
ANDREW PORTER <ANDREW.PORTER1039@GMAIL.COM>
2017-09-06 15:10:44 -0400

    user authentication tests written for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   root 'murals#index'
   # this will need to be changed back to 'static_pages#index' when we add react
   resources :murals, only: [:new, :create, :index, :show]
