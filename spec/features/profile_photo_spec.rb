598292fb4392b70a6972a3a9f353f7c18ca8df63
ANDREW PORTER <ANDREW.PORTER1039@GMAIL.COM>
2017-09-08 16:07:37 -0400

    tests for profile upload passing

--- spec/features/profile_photo_spec.rb
+++ spec/features/profile_photo_spec.rb
@@ -4,15 +4,12 @@ feature "profile photo" do
   scenario "user uploads a profile photo" do
     visit root_path
     click_link "Sign Up"
-
+    fill_in "Username", with: "andrew"
     fill_in "Email", with: "ash@s-mart.com"
     fill_in "Password", with: "boomstick!3vilisd3ad"
     fill_in "Password Confirmation", with: "boomstick!3vilisd3ad"
-    binding.pry
-    attach_file :profile_photo, "#{Rails.root}/spec/support/images/photo.png"
+    attach_file :user_profile_photo, File.join(Rails.root, 'spec', 'support', 'images', 'cat-bonnet_small.jpg')
     click_button "Sign up"
-
-    expect(page).to have_content("Welcome! Thanks for joining!")
-    expect(page).to have_css("img[src*='photo.png']")
+    expect(page).to have_css("img[src*='cat-bonnet_small.jpg']")
   end
 end
