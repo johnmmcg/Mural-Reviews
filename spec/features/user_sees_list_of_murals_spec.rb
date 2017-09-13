4cdcb88ccccdb0d7d8c6f482fe6098a5e0de8684
John <john.maurer.mcguinness@gmail.com>
2017-09-13 16:37:28 -0400

    styling nav bar and main header

--- spec/features/user_sees_list_of_murals_spec.rb
+++ spec/features/user_sees_list_of_murals_spec.rb
@@ -3,17 +3,12 @@ require 'rails_helper'
 feature "user sees a list of murals" do
   scenario "sees a list of mural photos and link for new mural" do
 
-
-
-
     user = User.create(username: 'johnmcg', email: 'johnmcg@gmail.com', password: 'password')
     big_fish = Mural.create(name: 'Big Fish', location: 'Martin Luther King Dr', user_id: user.id, description: "This is a description", rating: '4', photo_url: 'http://www.petsmart.com/learning-center/fish-care/the-right-food-to-feed-your-fish/A0009.html')
     mountain = Mural.create(name: 'Mountain', location: 'Walnut St', user_id: user.id, description: "This is another description", rating: '2', photo_url: 'https://pixabay.com/en/prairie-highway-the-scenery-679016/')
     visit root_path
     expect(page).to have_link "Big Fish"
     expect(page).to have_link "Mountain"
-    expect(page).to have_content "Number of Reviews:"
-    expect(page).to have_content "Average Rating:"
     expect(page).to have_content "Add New Mural"
   end
 
