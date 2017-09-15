018b829f3059b207309048e9252333b54ae0fdfe
John <john.maurer.mcguinness@gmail.com>
2017-09-14 22:28:07 -0400

    update index styling 3

--- react/src/components/MuralIndexTile.js
+++ react/src/components/MuralIndexTile.js
@@ -13,12 +13,12 @@ class MuralIndexTile extends Component {
     return(
       <div className="small-12 medium-6 large-4 columns">
         <div>
-          <Link to={`/murals/${this.props.id}`}>
             <div className="muralIndexTile">
-              <img src={this.props.photo_url} />
-              <h3>{this.props.name}</h3>
+              <Link to={`/murals/${this.props.id}`}>
+                <h3>{this.props.name}</h3>
+                <img src={this.props.photo_url} />
+              </Link>
             </div>
-          </Link>
         </div>
       </div>
     )
